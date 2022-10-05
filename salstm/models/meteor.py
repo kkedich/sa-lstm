47c29555e91f091d972a9faea827257a4bd4532d
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-05 00:22:01 +0000

    Adding numeric_owner as keyword arguement

--- salstm/models/meteor.py
+++ salstm/models/meteor.py
@@ -62,7 +62,7 @@ class Meteor:
                     if not is_within_directory(path, member_path):
                         raise Exception("Attempted Path Traversal in Tar File")
             
-                tar.extractall(path, members, numeric_owner) 
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
                 
             
             safe_extract(tar, path=destination_folder)
